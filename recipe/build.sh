830823a48a3936cb77c66c1c7d60ad50c6410063
Pavel Zwerschke <pavelzw@gmail.com>
2024-10-24 16:39:41 +0200

    set openssl path

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,7 @@ export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+export OPENSSL_DIR=$PREFIX
+
 cargo install --no-track --locked --root "$PREFIX" --path .
